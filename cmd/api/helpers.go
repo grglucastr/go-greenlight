733ddc968cc47cf69ae851909e562e80127331c2
George Nunes <georgebnunes@gmail.com>
2025-06-30 13:04:15 -0300

    refactor: helper to fetch ID from parameters

---
+++ cmd/api/helpers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+
+	// retrieve a slice containing these parameter names and values
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
