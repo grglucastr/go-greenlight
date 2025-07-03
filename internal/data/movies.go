9e7ccdb7fb4c1ece2c2fbe070b3cb8c9afc70cd7
George Nunes <georgebnunes@gmail.com>
2025-07-02 23:52:15 -0300

    feat: enconded strtucts as JSON

---
+++ internal/data/movies.go
@@ -0,0 +1,16 @@
+package data
+
+import "time"
+
+// All fields are exported (Capital Letter), necessary to
+// be visible because of 'enconding/json'
+
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitzero"`
+	Runtime   int32     `json:"runtime,omitzero"`
+	Genres    []string  `json:"genres,omitzero"`
+	Version   int32     `json:"version"`
+}
