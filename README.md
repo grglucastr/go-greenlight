075712b83f6b75a9bfb70d171ddab647c73fbcf4
George Nunes <georgebnunes@gmail.com>
2025-07-07 17:45:17 -0300

    feat: add connection to the database

--- README.md
+++ README.md
@@ -3,6 +3,29 @@
 Practice Go course of Alex Edwards - Let's Go Further. Consists in a JSON API managing information about movies.
 
 ## Project stack
+- Go 1.24
+- Postgres 
+
+## Requirements
+
+Before you run this project you have to have the Go installed on your machine.
+
+### Create database in Postgres
+
+This project was developed using the Postgres database. You have to provide it to your deployment environment.
+
+After that, make sure to create the database called `greenlight`.
+
+```
+$ postgres# CREATE DATABASE greenlight;
+```
+
+### Environment Variable
+Please make sure to add an environment variable called `GREENLIGHT_DB_DSN`. It should contain a connection string to your database just like this:
+
+```
+postgres://<user>:<pass>@<server>/greenlight?sslmode=disable
+``` 
 
 ## Endpoints
 
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -15,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Application dependency injection to be used in
@@ -25,14 +32,26 @@ type application struct {
 }
 
 func main() {
-	
+
 	var cfg config
 
 	flag.IntVar(&cfg.port, "port", 4000, "HTTP server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development, staging, production)")
+	flag.StringVar(&cfg.db.dsn, "db-dns", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(cfg)
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	app := &application{
 		config: cfg,
@@ -40,17 +59,36 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: app.routes(),
-		IdleTimeout: time.Minute,
-		ReadTimeout: 5 * time.Second,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
 	}
 
-	logger.Info("Starting server",  "addr", srv.Addr, "env", cfg.env)
+	logger.Info("Starting server", "addr", srv.Addr, "env", cfg.env)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
