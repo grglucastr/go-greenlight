2a14dd195c87a299763a7300d8ddf6108e059e98
George Nunes <georgebnunes@gmail.com>
2025-07-08 17:39:36 -0300

    feat: models container

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/grglucastr/go-greenlight/internal/data"
 	_ "github.com/lib/pq"
 )
 
@@ -35,6 +36,7 @@ type config struct {
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -68,6 +70,7 @@ func main() {
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
