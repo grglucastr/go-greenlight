d79ca7e521ab78098ff9fb8ca435a3332bd2f2c5
George Nunes <georgebnunes@gmail.com>
2025-08-24 15:00:49 -0300

    feat: setting rate limiter by client

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -31,21 +33,39 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
 
-	// Any code here will run only once, when we wrap something with middleware.
-
-	// Initialize a new rate limiter which allows an average of 2 requests per second,
-	// with a maximum of 4 requests in a single 'burst'
-	limiter := rate.NewLimiter(2, 4)
+	// declare a mutex and a map to hold the client's IP addresses and rate limiters.
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*rate.Limiter)
+	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Any code here will run for every request that the middleware handles.
+		// use the realip.FromRequest() function to get the client's IP address.
+		ip := realip.FromRequest(r)
 
-		//call limiter.Allow() to see if the request is permitted, and if it's not
-		// then we call the rateLimitExceededResponse() helper to return a 429 too many requests
-		if !limiter.Allow() {
+		// lock the mutex to prevent this code from being executed concurrently
+		mu.Lock()
+
+		// check to see if the IP address already exists in the map. If it doesn't, then
+		// initialize a new rate limiter and add the IP address and limiter on the map
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		// call the allow() method on the rate limiter for the current IP address.
+		// If the request isn't allowed, unlock the mutex and send a 429 Too Many Requests
+		// response, just like before.
+		if !clients[ip].Allow() {
+			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		// very importantly, unlock the mutext before calling the next hander in the chain.
+		// Notice that we DON'T use defer to unlock the mutex, as that would mean that the
+		// mutex isn't unlocked until all the handlers downstream of this middleware have
+		// also returned.
+		mu.Unlock()
 
 		next.ServeHTTP(w, r)
 	})
